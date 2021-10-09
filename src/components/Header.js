dcfb02c65e0d593106574e5599124b69bc976d2c
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-10-09 16:17:51 +0700

    header sticker, res

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,9 @@ const Header =()=>{
     return(
         <header className="header">
             <h2 className="header__title">launching open event!</h2>
-            <h1 className="header__heading">what<span className="header__bubble">!!!!!</span><br/>h<span className="header__triangle">a</span>ppened!<br/>mar-ket<span className="header__thirsties">thirties</span></h1>
+            <h1 className="header__heading"><span className="header__star">w</span>ha<span className="header__bubble">t</span><br/>
+            <span className="header__triangle">h</span>ppened!<br/>
+            mar-k<span className="header__thirsties">e</span>t</h1>
             <div className="header__nav">
                 <button className="header__nav--button">lorem ipsum</button>
                 <span className="header__nav--arrow">→</span>
